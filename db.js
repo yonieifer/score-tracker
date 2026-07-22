57f57338de6bbc0ef523de60a36f206cf42d2899
Yonatan Eiferman <yonieifer@gmail.com>
2026-07-22 10:54:25 +0300

    add db connection

--- db.js
+++ db.js
@@ -1,15 +1,15 @@
 import { MongoClient } from "mongodb";
 
-const client = new MongoClient(process.env.MONGO_URI)
+const client = new MongoClient(process.env.MDB_URI)
 
 const connectToMongo = async () => {
     try {
         await client.connect()
-        const db = client.db("test")
+        const db = client.db("score-tracker")
         console.log("connected to MongoDB...");
         return db
     } catch (error) {
-        throw new Error("failed to connect client")
+        throw new Error("failed to connect client", error)
     }
 }
 
