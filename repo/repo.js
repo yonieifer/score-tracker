716df713c28d04332cb4842e3840fee1d6c7ae40
Yonatan Eiferman <yonieifer@gmail.com>
2026-07-22 10:53:56 +0300

    add repo

---
+++ repo/repo.js
@@ -0,0 +1,16 @@
+import db from "../db.js"
+
+
+const collection = db.collection("scores")
+
+export const create = async (data) => {
+    const result = await collection.insertOne(data)
+    return result.insertedId.toString()
+}
+
+export const read = async (filter={}, sort={}, limit=0) => {
+    const result = await collection.find(filter).sort(sort).limit(limit)
+    return result
+}
+
+
