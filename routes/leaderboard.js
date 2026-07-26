1d59145d8eae9007b31022ddfe7954c7b495c875
Yonatan Eiferman <yonieifer@gmail.com>
2026-07-26 11:20:28 +0300

    fix endpoints order in leaderboard router

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -3,15 +3,16 @@ import {getGameTopTen, getGlobalTopTen } from "../service/service.js"
 
 const router = express.Router()
 
+router.get("/global", async (req, res) => {
+    const topTen = await getGlobalTopTen()
+    res.send(topTen)
+})
+
 router.get("/:game", async (req, res) => {
     const {game} = req.params
     const topTen = await getGameTopTen(game)
     res.send(topTen)
 })
 
-router.get("/global", async (req, res) => {
-    const topTen = await getGlobalTopTen()
-    res.send(topTen)
-})
 
 export default router
