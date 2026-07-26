b740c56223e1bf7b3ea234e2a264c4bb8f09f08e
Yonatan Eiferman <yonieifer@gmail.com>
2026-07-26 10:20:09 +0300

    add scores router

---
+++ routes/scores.js
@@ -0,0 +1,13 @@
+import express from "express"
+import { createScore } from "../service/service.js"
+
+const router = express.Router()
+
+router.post("/", async (req, res, next) => {
+    const { playerName, game, points } = req.body
+    if (!playerName || !game || !points) {
+        throw new Error("body missing parameter")
+    }
+    const id = await createScore({playerName, game, points})
+    res.status(201).send(`creted | id: ${id}`)
+})
