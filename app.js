16ec9c441ecc53c6c8cc707ac737a8a0025544eb
Yonatan Eiferman <yonieifer@gmail.com>
2026-07-26 11:41:24 +0300

    add error handling

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import scoresRouter from "./routes/scores.js";
 import leaderboardRouter from "./routes/leaderboard.js";
+import { getPlayerScores } from "./service/service.js";
 
 const app = express();
 
@@ -9,10 +10,21 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(express.json())
+app.use(express.json());
 
 app.use("/scores", scoresRouter);
 
 app.use("/leaderboard", leaderboardRouter);
+
+app.get("/player/:name", async (req, res) => {
+    const { name } = req.params;
+    const scores = await getPlayerScores(name)
+    res.send(scores)
+});
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(err.statusCode || 500).send(err.message || "server internal error")
+})
 
 app.listen(3000, () => console.log("listening..."));
